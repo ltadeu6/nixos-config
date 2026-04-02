68cde1000add934f533118e7953dbd7a5c7eb65e
ltadeu6 <ltadeu6@protonmail.com>
2026-04-02 12:17:10 -0300

    Declare gtk theme and cursor

--- home/ltadeu6.nix
+++ home/ltadeu6.nix
@@ -109,6 +109,43 @@
     };
   };
 
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+    iconTheme = {
+      name = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+    };
+    font = {
+      name = "Sans";
+      size = 10;
+    };
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+      gtk-toolbar-style = "GTK_TOOLBAR_ICONS";
+      gtk-toolbar-icon-size = "GTK_ICON_SIZE_LARGE_TOOLBAR";
+      gtk-button-images = 0;
+      gtk-menu-images = 0;
+      gtk-enable-event-sounds = 1;
+      gtk-enable-input-feedback-sounds = 0;
+      gtk-xft-antialias = 1;
+      gtk-xft-hinting = 1;
+      gtk-xft-hintstyle = "hintmedium";
+      gtk-xft-rgba = "rgb";
+    };
+  };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    name = "Breeze";
+    package = pkgs.kdePackages.breeze;
+    size = 30;
+  };
+
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
@@ -167,8 +204,6 @@
 
   home.sessionVariables = {
     MANPAGER = "most";
-    XCURSOR_THEME = "Breeze";
-    XCURSOR_SIZE = "30";
     HYPRCURSOR_THEME = "Breeze";
     HYPRCURSOR_SIZE = "30";
     QT_QPA_PLATFORMTHEME = "qt6ct";
