0e6e268f8751f68436ccd660a7879cb239203376
ltadeu6 <ltadeu6@protonmail.com>
2026-05-02 16:37:21 -0300

    Simplify antimicrox disable script

--- hosts/Nixos/configuration.nix
+++ hosts/Nixos/configuration.nix
@@ -788,10 +788,7 @@ in {
     mode = "0755";
     text = ''
       #!/bin/sh
-      ${pkgs.antimicrox}/bin/antimicrox --unload 1 >/dev/null 2>&1 || true
-      ${pkgs.procps}/bin/pkill -x .antimicrox-wrapped >/dev/null 2>&1 || true
-      ${pkgs.procps}/bin/pkill -x antimicrox >/dev/null 2>&1 || true
-      exec ${pkgs.procps}/bin/pkill -x AntiMicroX
+      exec ${pkgs.procps}/bin/pkill -x .antimicrox-wrapped
     '';
   };
 
