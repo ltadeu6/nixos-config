c67e1a7a712db08d46f808106177f5168b31cdcb
ltadeu6 <ltadeu6@protonmail.com>
2026-05-25 18:05:47 -0300

    Add CORS header for Forgejo API via nginx (fix homepage project fetch)

--- hosts/NixOracle/configuration.nix
+++ hosts/NixOracle/configuration.nix
@@ -112,6 +112,15 @@ in {
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         '';
       };
+      locations."/api/" = {
+        proxyPass = "http://127.0.0.1:3000";
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          add_header 'Access-Control-Allow-Origin' 'https://tadix.dev' always;
+        '';
+      };
     };
     virtualHosts."biolab.tadix.dev" = {
       enableACME = true;
