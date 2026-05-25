6e293a5c8da86afed98ca4e8a9a279f170a23d5f
ltadeu6 <ltadeu6@protonmail.com>
2026-05-25 17:28:36 -0300

    Use nextcloud32 (nextcloud31 is EOL in nixpkgs 25.11)

--- hosts/NixOracle/configuration.nix
+++ hosts/NixOracle/configuration.nix
@@ -136,7 +136,7 @@ in {
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud31;
+    package = pkgs.nextcloud32;
     hostName = "nextcloud.tadix.dev";
     https = true;
     database.createLocally = true;
