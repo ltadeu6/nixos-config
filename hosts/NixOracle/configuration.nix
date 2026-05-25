bd9063bae489f6e7407ae0188b5f221eff8c0b5a
ltadeu6 <ltadeu6@protonmail.com>
2026-05-25 18:35:44 -0300

    Remove Forgejo CORS and nginx /api/ location (no longer needed)

--- hosts/NixOracle/configuration.nix
+++ hosts/NixOracle/configuration.nix
@@ -113,15 +113,6 @@ in {
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         '';
       };
-      locations."/api/" = {
-        proxyPass = "http://127.0.0.1:3000";
-        extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          add_header 'Access-Control-Allow-Origin' 'https://tadix.dev' always;
-        '';
-      };
     };
     virtualHosts."biolab.tadix.dev" = {
       enableACME = true;
@@ -213,11 +204,6 @@ in {
       };
       service.DISABLE_REGISTRATION = true;
       log.LEVEL = "Warn";
-      cors = {
-        ENABLED = true;
-        ALLOW_DOMAIN = "tadix.dev";
-        METHODS = "GET,HEAD,OPTIONS";
-      };
     };
   };
 
